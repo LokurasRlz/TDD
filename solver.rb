c203589719e58a7b92e47aa162a00cbd7371c553
Matias Aguirre <aguirremati09@gmail.com>
2022-11-10 16:08:37 -0300

    Update Project

---
+++ solver.rb
@@ -0,0 +1,13 @@
+class Solver
+  def factorial(num)
+    return raise 'Error, arguement for factorial needs to be a +ve Int type' if !num.is_a?(Integer) || num.negative?
+    return 1 if num.zero?
+
+    num * (factorial num - 1)
+  end
+
+  def reverser(str)
+    raise 'Expect argument for reverser to be a non empty string' if !str.is_a?(String) || !str.length.positive?
+
+    str.reverse
+  end
